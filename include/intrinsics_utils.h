737b51b5ab75c6a2b93bd7ea59adf55a17de448a
Benjamin Parker <bqp@mailbox.org>
2026-02-13 08:23:45 -0800

    bug fixes and implementation changes for macro imm8 masks, removal of unused functions

--- include/intrinsics_utils.h
+++ include/intrinsics_utils.h
@@ -2,6 +2,27 @@
 #define INTRINSICS_UTILS
 
 #include <immintrin.h>
+#include <stdint.h>
+
+// Macros for imm8 constants for permuting/shuffling.
+#define INT8_ALLBITS ((int32_t)0xff)
+
+#define LPERM0 0xe4
+#define LPERM1 0x39
+#define LPERM2 0x4e
+#define LPERM3 0x93
+
+#define M128_LPERM_MASK (LPERM0 + (LPERM1 << 8) + (LPERM2 << 16) + (LPERM3 << 24))
+#define M128_LPERM_TO_IMM8(NPERMS) (((M128_LPERM_MASK) >> ((NPERMS) * 8)) & INT8_ALLBITS)
+
+#define RPERM0 LPERM0
+#define RPERM1 LPERM3
+#define RPERM2 LPERM2
+#define RPERM3 LPERM1
+
+#define M128_RPERM_MASK (RPERM0 + ((RPERM1) << 8) + ((RPERM2) << 16) + ((RPERM3) << 24))
+#define M128_RPERM_TO_IMM8(NPERMS) (((M128_RPERM_MASK) >> ((NPERMS) * 8)) & INT8_ALLBITS)
+
 
 //----------------------------------------------------------------------------
 // Functions for setting values of arrays.
@@ -54,12 +75,15 @@ float _mm_register_min_ps(__m128);
 float _mm256_register_min_ps(__m256);
 double _mm_register_min_pd(__m128d);
 double _mm256_register_min_pd(__m256d);
-int _mm256_masksltnnz_epi64(__m256i, __m256i);
+
+
+//----------------------------------------------------------------------------
+// Functions for permuting elements in registers.
+//----------------------------------------------------------------------------
+
 __m256d _mm256_leftperm_pd(__m256d, int);
 __m256d _mm256_rightperm_pd(__m256d, int);
 __m256i _mm256_leftperm_epi64(__m256i, int);
-__m256i _mm256_maskleftperm_epi64(__m256i, __m256i);
-__m256i _mm256_leftshift_epi64(__m256i, int);
 __m256i _mm256_leftperm_epi32(__m256i, int);
 
 //----------------------------------------------------------------------------
