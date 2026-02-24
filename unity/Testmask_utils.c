dce41984ba296cfcc463767af2895ba22af387ab
Benjamin Parker <bqp@mailbox.org>
2026-02-24 15:03:21 -0800

    new mask tests, cutoff index adjustments

--- unity/Testmask_utils.c
+++ unity/Testmask_utils.c
@@ -54,8 +54,11 @@ void test_mm_set_mask_epi32(void);
 void test_mm_set_mask_fromto_epi64(void);
 void test_mm_set_mask_epi64(void);
 
-
+void test_mm256_set_mask_fromto_epi32(void);
 void test_mm256_set_mask_epi32(void);
+
+void test_mm512_set_mask_fromto_epi32(void);
+void test_mm512_set_mask_epi32(void);
 
 // Functions for setting expected mask results in XMM registers.
 void m128_epi32_set_expected_fromto(int, int);
@@ -86,7 +89,12 @@ int main(int argc, char *argv[])
     RUN_TEST(test_mm_set_mask_fromto_epi32);
     RUN_TEST(test_mm_set_mask_epi32);
     RUN_TEST(test_mm_set_mask_epi64);
+
+    RUN_TEST(test_mm256_set_mask_fromto_epi32);
     RUN_TEST(test_mm256_set_mask_epi32);
+
+    RUN_TEST(test_mm512_set_mask_fromto_epi32);
+    RUN_TEST(test_mm512_set_mask_epi32);
 
     return UNITY_END();
 }
@@ -158,19 +166,19 @@ void test_mm_set_mask_fromto_epi32(void)
 
 void test_mm_set_mask_epi32(void)
 {
-    int cutoff;
+    int cutoff_index;
     __m128i result_mask;
     __m128i store_mask = _mm_set1_epi32(INT32_ALLBITS);
 
     for (int i = 0; i < M128_INDICES_LEN; i++) {
         // Set the index of the last nonzero element in the mask.
-        cutoff = m128_indices[i];
+        cutoff_index = m128_indices[i];
 
         // Set the expected result.
-        m128_epi32_set_expected_to(cutoff);
+        m128_epi32_set_expected_to(cutoff_index);
 
         // Store the actual result.
-        result_mask = _mm_set_mask_epi32(cutoff);
+        result_mask = _mm_set_mask_epi32(cutoff_index);
         _mm_maskstore_epi32(epi32_actual, store_mask, result_mask);
 
         TEST_ASSERT_EQUAL_INT32_ARRAY(epi32_expected, epi32_actual, INT32_PER_M128_REG);
@@ -184,47 +192,150 @@ void test_mm_set_mask_fromto_epi64(void)
 
 void test_mm_set_mask_epi64(void)
 {
-    int cutoff;
+    int cutoff_index;
     __m128i result_mask;
     __m128i store_mask = _mm_set1_epi64x(INT64_ALLBITS);
 
     for (int i = 0; i < M128_64BIT_INDICES_LEN; i++) {
         // Set the index of the last nonzero element in the mask.
-        cutoff = m128_indices[i];
+        cutoff_index = m128_indices[i];
 
         // Set the expected result.
-        m128_epi64_set_expected_to(cutoff);
+        m128_epi64_set_expected_to(cutoff_index);
 
         // Store the actual result.
-        result_mask = _mm_set_mask_epi64(cutoff);
+        result_mask = _mm_set_mask_epi64(cutoff_index);
         _mm_maskstore_epi64(epi64_actual, store_mask, result_mask);
 
-        TEST_ASSERT_EQUAL_INT64_ARRAY(epi64_expected, epi64_actual, INT32_PER_M128_REG);
+        TEST_ASSERT_EQUAL_INT64_ARRAY(epi64_expected, epi64_actual, INT64_PER_M128_REG);
+    }
+}
+
+void test_mm256_set_mask_fromto_epi32(void)
+{
+    int start, end;
+    __m256i result_mask;
+    __m256i store_mask = _mm256_set1_epi32(INT32_ALLBITS);
+
+    // Test results where the ending index is greater than or equal to the starting index.
+    for (int i = 0; i < M256_INDICES_LEN; i++) {
+        start = m256_indices[i];
+
+        for (int j = i; j < M256_INDICES_LEN; j++) {
+            end = m256_indices[j];
+
+            // Set the expected result.
+            m256_epi32_set_expected_fromto(start, end);
+
+            // Store the actual result.
+            result_mask = _mm256_setmask_fromto_epi32(start, end);
+            _mm256_maskstore_epi32(epi32_actual, store_mask, result_mask);
+
+            TEST_ASSERT_EQUAL_INT32_ARRAY(epi32_expected, epi32_actual, INT32_PER_M256_REG);
+        }
+    }
+
+    // Test results where the ending index is less than the starting index.
+    for (int i = 0; i < M256_INDICES_LEN; i++) {
+        start = m256_indices[i];
+
+        for (int j = i - 1; j >= 0; j--) {
+            end = m256_indices[j];
+
+            // Set the expected result.
+            m256_epi32_set_expected_fromto(start, end);
+
+            // Store the actual result.
+            result_mask = _mm256_setmask_fromto_epi32(start, end);
+            _mm256_maskstore_epi32(epi32_actual, store_mask, result_mask);
+
+            TEST_ASSERT_EQUAL_INT32_ARRAY(epi32_expected, epi32_actual, INT32_PER_M256_REG);
+        }
     }
 }
 
 void test_mm256_set_mask_epi32(void)
 {
-    int cutoff;
+    int cutoff_index;
     __m256i result_mask;
     __m256i store_mask = _mm256_set1_epi32(INT32_ALLBITS);
 
     for (int i = 0; i < M256_INDICES_LEN; i++) {
         // Set the index of the last nonzero element in the mask.
-        cutoff = m256_indices[i];
+        cutoff_index = m256_indices[i];
 
         // Set the expected result.
-        m256_epi32_set_expected_to(cutoff);
+        m256_epi32_set_expected_to(cutoff_index);
 
         // Store the actual result.
-        result_mask = _mm256_set_mask_epi32(cutoff);
+        result_mask = _mm256_set_mask_epi32(cutoff_index);
         _mm256_maskstore_epi32(epi32_actual, store_mask, result_mask);
 
         TEST_ASSERT_EQUAL_INT32_ARRAY(epi32_expected, epi32_actual, INT32_PER_M256_REG);
     }
 }
 
+void test_mm512_set_mask_fromto_epi32(void)
+{
+    int start, end;
+    __mmask16 result_mask;
 
+    // Test results where the ending index is greater than or equal to the starting index.
+    for (int i = 0; i < M512_INDICES_LEN; i++) {
+        start = m512_indices[i];
+
+        for (int j = i; j < M512_INDICES_LEN; j++) {
+            end = m512_indices[j];
+
+            // Set the expected result.
+            m512_epi32_set_expected_fromto(start, end);
+
+            // Store the actual result.
+            result_mask = _mm512_setmask_fromto_epi32(start, end);
+            _mm512_storeu_epi32(epi32_actual, _mm512_movm_epi32(result_mask));
+
+            TEST_ASSERT_EQUAL_INT32_ARRAY(epi32_expected, epi32_actual, INT32_PER_M512_REG);
+        }
+    }
+
+    // Test results where the ending index is less than the starting index.
+    for (int i = 0; i < M512_INDICES_LEN; i++) {
+        start = m512_indices[i];
+
+        for (int j = i - 1; j >= 0; j--) {
+            end = m512_indices[j];
+
+            // Set the expected result.
+            m512_epi32_set_expected_fromto(start, end);
+
+            // Store the actual result.
+            result_mask = _mm512_setmask_fromto_epi32(start, end);
+            _mm512_storeu_epi32(epi32_actual, _mm512_movm_epi32(result_mask));
+
+            TEST_ASSERT_EQUAL_INT32_ARRAY(epi32_expected, epi32_actual, INT32_PER_M512_REG);
+        }
+    }
+}
+
+void test_mm512_set_mask_epi32(void)
+{
+    int cutoff_index;
+    __mmask16 result_mask;
+
+    for (int i = 3; i < M512_INDICES_LEN; i++) {
+        // Set the index of the last nonzero element in the mask.
+        cutoff_index = m512_indices[i];
+
+        // Set the expected result.
+        m512_epi32_set_expected_to(cutoff_index);
+
+        // Store the actual result.
+        result_mask = _mm512_set_mask_epi32(cutoff_index);
+        _mm512_storeu_epi32(epi32_actual, _mm512_movm_epi32(result_mask));
+
+        TEST_ASSERT_EQUAL_INT32_ARRAY(epi32_expected, epi32_actual, INT32_PER_M512_REG);
+    }
+}
 
 //----------------------------------------------------------------------------
 // Functions for setting expected mask results in XMM registers.
@@ -255,9 +366,9 @@ void m128_epi32_set_expected_fromto(int from, int to)
     }
 }
 
-void m128_epi32_set_expected_to(int cutoff)
+void m128_epi32_set_expected_to(int cutoff_index)
 {
-    m128_epi32_set_expected_fromto(0, cutoff);
+    m128_epi32_set_expected_fromto(0, cutoff_index);
 }
 
 void m128_epi64_set_expected_fromto(int from, int to)
@@ -285,23 +396,23 @@ void m128_epi64_set_expected_fromto(int from, int to)
     }
 }
 
-void m128_epi64_set_expected_to(int cutoff)
+void m128_epi64_set_expected_to(int cutoff_index)
 {
-    m128_epi64_set_expected_fromto(0, cutoff);
+    m128_epi64_set_expected_fromto(0, cutoff_index);
 }
 
-void m128_ps_set_expected_to(int cutoff)
+void m128_ps_set_expected_to(int cutoff_index)
 {
-    m128_epi32_set_expected_to(cutoff);
+    m128_epi32_set_expected_to(cutoff_index);
     __m128i lmask = _mm_set1_epi32(INT32_ALLBITS);
     __m128i regi = _mm_maskload_epi32(epi32_expected, lmask);
     __m128 regs = _mm_castsi128_ps(regi);
     _mm_store_ps(ps_expected, regs);
 }
 
-void m128_pd_set_expected_to(int cutoff)
+void m128_pd_set_expected_to(int cutoff_index)
 {
-    m128_epi64_set_expected_to(cutoff);
+    m128_epi64_set_expected_to(cutoff_index);
     __m128i lmask = _mm_set1_epi64x(INT64_ALLBITS);
     __m128i regi = _mm_maskload_epi64(epi64_expected, lmask);
     __m128d regs = _mm_castsi128_pd(regi);
@@ -337,9 +448,9 @@ void m256_epi32_set_expected_fromto(int from, int to)
     }
 }
 
-void m256_epi32_set_expected_to(int cutoff)
+void m256_epi32_set_expected_to(int cutoff_index)
 {
-    m256_epi32_set_expected_fromto(0, cutoff);
+    m256_epi32_set_expected_fromto(0, cutoff_index);
 }
 
 void m256_epi64_set_expected_fromto(int from, int to)
@@ -367,23 +478,23 @@ void m256_epi64_set_expected_fromto(int from, int to)
     }
 }
 
-void m256_epi64_set_expected_to(int cutoff)
+void m256_epi64_set_expected_to(int cutoff_index)
 {
-    m256_epi64_set_expected_fromto(0, cutoff);
+    m256_epi64_set_expected_fromto(0, cutoff_index);
 }
 
-void m256_ps_set_expected_to(int cutoff)
+void m256_ps_set_expected_to(int cutoff_index)
 {
-    m256_epi32_set_expected_to(cutoff);
+    m256_epi32_set_expected_to(cutoff_index);
     __m256i lmask = _mm256_set1_epi32(INT32_ALLBITS);
     __m256i regi = _mm256_maskload_epi32(epi32_expected, lmask);
     __m256 regs = _mm256_castsi256_ps(regi);
     _mm256_store_ps(ps_expected, regs);
 }
 
-void m256_pd_set_expected_to(int cutoff)
+void m256_pd_set_expected_to(int cutoff_index)
 {
-    m256_epi64_set_expected_to(cutoff);
+    m256_epi64_set_expected_to(cutoff_index);
     __m256i lmask = _mm256_set1_epi64x(INT64_ALLBITS);
     __m256i regi = _mm256_maskload_epi64(epi64_expected, lmask);
     __m256d regs = _mm256_castsi256_pd(regi);
@@ -416,9 +527,9 @@ void m512_epi32_set_expected_fromto(int from, int to)
     }
 }
 
-void m512_epi32_set_expected_to(int cutoff)
+void m512_epi32_set_expected_to(int cutoff_index)
 {
-    m512_epi32_set_expected_fromto(0, cutoff);
+    m512_epi32_set_expected_fromto(0, cutoff_index);
 }
 
 void m512_epi64_set_expected_fromto(int from, int to)
@@ -446,8 +557,8 @@ void m512_epi64_set_expected_fromto(int from, int to)
     }
 }
 
-void m512_epi64_set_expected_to(int cutoff)
+void m512_epi64_set_expected_to(int cutoff_index)
 {
-    m512_epi64_set_expected_fromto(0, cutoff);
+    m512_epi64_set_expected_fromto(0, cutoff_index);
 }
 #endif 
